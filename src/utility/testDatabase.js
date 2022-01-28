c77e77e8000b06fb5e209f6daa4c6d14de54a4af
Giorgio <giorgio.cesaroni@me.com>
2022-01-28 09:15:14 +0100

    testDatabase edit

--- src/utility/testDatabase.js
+++ src/utility/testDatabase.js
@@ -3,30 +3,30 @@ export const testDatabase = {
     "notes": "Pizza with Joe",
     "price": 18.59,
     "category": "🍕",
-    "date": "Thu Jan 13 2022 13:06:14 GMT+0100 (CET)"
+    "date": "Jan 13 2022 13:06:14 GMT+0100 (CET)"
   },
   "RZOHyAydImg8tZ9zscqw": {
     "notes": "Jane's car",
     "price": 25.98,
     "category": "⛽️",
-    "date": "Thu Jan 13 2022 13:06:14 GMT+0100 (CET)"
+    "date": "Jan 14 2022 13:06:14 GMT+0100 (CET)"
   },
   "RNOHyAydImg8tZ9zFcqw": {
     "notes": "Screwdriver",
     "price": 8.21,
     "category": "⚙️",
-    "date": "Thu Jan 13 2022 13:06:14 GMT+0100 (CET)"
+    "date": "Jan 15 2022 13:06:14 GMT+0100 (CET)"
   },
   "RNOHyA5dImg8tZ9zFcqz": {
     "notes": "Fresh market",
     "price": 109.34,
     "category": "🥑",
-    "date": "Thu Jan 13 2022 13:06:14 GMT+0100 (CET)"
+    "date": "Jan 16 2022 13:06:14 GMT+0100 (CET)"
   },
   "RNOHyAydImg8tZsfFcqw": {
     "notes": "Income",
     "price": 1000.00,
     "category": "💵",
-    "date": "Thu Jan 13 2022 13:06:14 GMT+0100 (CET)"
+    "date": "Jan 17 2022 13:06:14 GMT+0100 (CET)"
   },
 }
