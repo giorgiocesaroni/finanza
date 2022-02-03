6b7456f151edbd3fc7108b90f8ccccd972fca6a5
Giorgio <giorgio.cesaroni@me.com>
2022-02-03 14:00:11 +0100

    Updates

---
+++ precache-manifest.745e39c30ab577ac45a0020e995d0864.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d604d7e8ea81224dd61bb2e1021a88a",
+    "url": "/finanza/index.html"
+  },
+  {
+    "revision": "a5be503e971aa1bdd2c0",
+    "url": "/finanza/static/css/main.65cee786.chunk.css"
+  },
+  {
+    "revision": "af059902caeafacdfac7",
+    "url": "/finanza/static/js/2.6b227958.chunk.js"
+  },
+  {
+    "revision": "379c4f6b321b3284ab06cb3e622010c9",
+    "url": "/finanza/static/js/2.6b227958.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5be503e971aa1bdd2c0",
+    "url": "/finanza/static/js/main.52357c7c.chunk.js"
+  },
+  {
+    "revision": "effd00cdac8611ff37da",
+    "url": "/finanza/static/js/runtime-main.ff181769.js"
+  }
+]);
